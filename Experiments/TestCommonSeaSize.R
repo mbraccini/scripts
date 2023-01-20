6dc94b05d22d4a90f0ab3322c498fa4500ff0239
Michele <miki892@gmail.com>
2023-01-20 17:25:49 +0100

    esperimenti per vedere se la common sea crewsca al crescere del numero di nodi

--- Experiments/TestCommonSeaSize.R
+++ Experiments/TestCommonSeaSize.R
@@ -14,29 +14,28 @@ simulateNet <- function(bn, initialStates) {
 set.seed(2)
 k       <- 2
 bias    <- 0.5
-noNetworks <- 100
+noNetworks <- 1000
 
 stats <- list()
-for (noNodes in seq(5,14)){
+start <- 2 
+end <- 20
+for (noNodes in seq(start,end)){
     print(noNodes)
     res <- vector("integer", length = noNetworks)
 
     for(i in seq_len(noNetworks)){
-        noInitialStates <- (2^noNodes)/2
-        initialStates <- lapply(rep(1, noInitialStates), function(x) rndBooleanState(noNodes))
-
-        net <- rbn(noNodes, k, bias, selfLoops=FALSE)
-        #saveRDS(net, file = glue('{folder}/bn_{i}.RData'))
-        bn  <- toBoolNet(net, glue('{subFolderSL_0}/bn_{i}'))
-
-            
-        res[i] <-   simulateNet(bn, initialStates)$commonSeaSize
-       
+        bn <- generateRandomNKNetwork(n=noNodes, k=k, functionGeneration = "biased", zeroBias=bias)
+        atts <- getAttractors(bn, type = "synchronous") 
+        res[i] <- (commonSea(atts)$commonSeaSize)/noNodes
     }
     stats[[length(stats) +1 ]] <- res
 }
 
-boxplot(stats)
+pdf(glue('CommonSeaSizeProgression.pdf'))
+boxplot(stats,xaxt = "n", ylab="Common sea size / no. nodes", main="Size progression of the common sea")
+axis(1, at = seq(start - 1, end - 1), labels = seq(start, end)) # axis, ticks
+mtext('no. nodes', side=1, line=3)
+dev.off()
 
 #lapply(res, `[[`, "commonSeaSize")
 #pippo<-readRDS("net_1.RData")
