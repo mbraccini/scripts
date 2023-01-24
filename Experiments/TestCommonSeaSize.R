1d2696eae4ef8e9d08cceb39fe13e2133b1168b7
Michele <miki892@gmail.com>
2023-01-24 10:46:01 +0100

    esperimenti per vedere se la common sea crewsca al crescere del numero di nodi

--- Experiments/TestCommonSeaSize.R
+++ Experiments/TestCommonSeaSize.R
@@ -14,26 +14,27 @@ simulateNet <- function(bn, initialStates) {
 set.seed(2)
 k       <- 2
 bias    <- 0.5
-noNetworks <- 1000
+noNetworks <- 100
 
 stats <- list()
-start <- 2 
-end <- 20
-for (noNodes in seq(start,end)){
+start <- 20
+end <- 28
+for (noNodes in seq(start,end,by=2)){
     print(noNodes)
     res <- vector("integer", length = noNetworks)
 
     for(i in seq_len(noNetworks)){
         bn <- generateRandomNKNetwork(n=noNodes, k=k, functionGeneration = "biased", zeroBias=bias)
-        atts <- getAttractors(bn, type = "synchronous") 
+        #atts <- getAttractors(bn, type = "synchronous") 
+        atts <- getAttractors(bn, type = "synchronous", method="random",startStates=as.integer((2^noNodes) / 2000) ) 
         res[i] <- (commonSea(atts)$commonSeaSize)/noNodes
     }
     stats[[length(stats) +1 ]] <- res
 }
 
-pdf(glue('CommonSeaSizeProgression.pdf'))
+pdf(glue('CommonSeaSizeProgression20-28_sampled.pdf'))
 boxplot(stats,xaxt = "n", ylab="Common sea size / no. nodes", main="Size progression of the common sea")
-axis(1, at = seq(start - 1, end - 1), labels = seq(start, end)) # axis, ticks
+axis(1, at = seq(1,((end-start)/2)+1), labels = seq(start, end,by=2)) # axis, ticks
 mtext('no. nodes', side=1, line=3)
 dev.off()
 
