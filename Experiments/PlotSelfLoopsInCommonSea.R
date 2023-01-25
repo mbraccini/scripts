84cfb1ef1f822a94d1c45e10bf40fa05b90f9087
Michele <miki892@gmail.com>
2023-01-25 10:28:30 +0100

    plot self loops in common sea

---
+++ Experiments/PlotSelfLoopsInCommonSea.R
@@ -0,0 +1,20 @@
+library(glue)
+
+path="n100k2p05/"
+name <- "slInZeros"
+pdf(glue('SL_in_commonSea_{name}.pdf'))
+res     <- list()
+labels  <- list()
+for (SLNUMBER in c(5,10,20))
+{
+    res[[length(res)+1]] <- read.table(glue('{path}{name}_{SLNUMBER}_augmAND.txt'))[,1]
+    labels[[length(labels)+1]] <- glue('{SLNUMBER}_And')
+    res[[length(res)+1]] <- read.table(glue('{path}{name}_{SLNUMBER}_augmOR.txt'))[,1]
+    labels[[length(labels)+1]] <- glue('{SLNUMBER}_Or')
+    res[[length(res)+1]] <- read.table(glue('{path}{name}_{SLNUMBER}_augmRND.txt'))[,1]
+    labels[[length(labels)+1]] <- glue('{SLNUMBER}_Rnd')
+}
+
+boxplot(res, xaxt = "n", main="No. of selfloops in common sea with value 0", ylab="no. of selfloops")
+axis(1, at = seq(1,length(res)), las = 2,labels = labels) # axis, ticks
+dev.off()
