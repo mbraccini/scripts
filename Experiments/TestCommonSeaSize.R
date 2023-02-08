d29a126a4d64d88e0c52af4f2bce963b4ab26d8e
Michele <miki892@gmail.com>
2023-02-08 10:26:15 +0100

    update

--- Experiments/TestCommonSeaSize.R
+++ Experiments/TestCommonSeaSize.R
@@ -1,40 +1,38 @@
 library(BoolNet)
-source("bOOLnET.R")
-
-simulateNet <- function(bn, initialStates) {
-    initialStatesWithFixedGenesPerNetwork <- lapply(initialStates, FUN=function(x) applyFixedGenes(bn,x))
-    atts <- getAttractors(bn, 
-                            type = "synchronous", 
-                            method = "chosen", 
-                            startStates = initialStatesWithFixedGenesPerNetwork)
-    return(commonSea(atts))
-}
+source("../bOOLnET.R")
 
 
-set.seed(2)
+
+set.seed(42)
 k       <- 2
 bias    <- 0.5
 noNetworks <- 100
 
 stats <- list()
 start <- 30
-end <- 42
-for (noNodes in seq(start,end,by=2)){
+end <- 40
+mainFolder <- glue('testing')
+dir.create(mainFolder)
+
+for (noNodes in seq(start,end,by=5)){
     print(noNodes)
     res <- vector("integer", length = noNetworks)
 
     for(i in seq_len(noNetworks)){
-        bn <- generateRandomNKNetwork(n=noNodes, k=k, functionGeneration = "biased", zeroBias=bias)
+        net <- rbn(n=noNodes, k=k, p=bias, selfLoops=FALSE)
+        bn  <- toBoolNet(net, glue('{mainFolder}/bn_{noNodes}_{i}'))
+
+        #bn <- generateRandomNKNetwork(n=noNodes, k=k)
+        atts <- getAttractors(bn, type = "synchronous", method="random",startStates=as.integer((2^noNodes)/1000000)) 
         #atts <- getAttractors(bn, type = "synchronous") 
-        atts <- getAttractors(bn, type = "synchronous", method="random",startStates=as.integer((2^noNodes) / 1000000) ) 
         res[i] <- (commonSea(atts)$commonSeaSize)/noNodes
     }
     stats[[length(stats) +1 ]] <- res
 }
 
-pdf(glue('CommonSeaSizeProgression{start}-{end}_sampled.pdf'))
-boxplot(stats,xaxt = "n", ylab="Common sea size / no. nodes", main="Size progression of the common sea (1/1000000)")
-axis(1, at = seq(1,((end-start)/2)+1), labels = seq(start, end,by=2)) # axis, ticks
+pdf(glue('CommonSeaSizeProgression{start}-{end}_1000000_senzaatuoanelli.pdf'))
+boxplot(stats,xaxt = "n", ylab="Common sea size / no. nodes", main="Size progression of the common sea")
+axis(1, at = seq(1,((end-start)/5)+1), labels = seq(start, end,by=5)) # axis, ticks
 mtext('no. nodes', side=1, line=3)
 dev.off()
 
