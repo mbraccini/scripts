aa3bfbf2d6e8aadf7cf2ea86ccb39e51613401e0
Michele <miki892@gmail.com>
2023-04-17 11:15:04 +0200

    distribuzione delle distanze del caso 0sl

--- Experiments/PlotDistributionsOfDistances.R
+++ Experiments/PlotDistributionsOfDistances.R
@@ -1,8 +1,31 @@
 library(glue)
 
-type = "allFunctions" 
-#type = "noTRUE_FALSE_XOR_XNOR"
+#type = "allFunctions" 
+type = "noTRUE_FALSE_XOR_XNOR"
 path=glue('n100k2p05_pseudoAttractors_{type}_31Marzo23')
+
+# 0 autonelli
+pdf(glue('DISTANCES_{type}_sl_0.pdf'))
+file=glue('{path}/distances_sl_0.csv')
+        df<- read.table(file,sep=",",fill=TRUE)
+        ls <- unlist(df)
+        distances <- ls[!is.na(ls)]
+        #hist(distances, breaks=0:(max(distances)+1), right=FALSE, freq=FALSE)
+        tmp <- hist(distances, 
+                    breaks=0:(max(distances)+1), 
+                    xaxt="n", 
+                    right=FALSE, 
+                    freq=TRUE,
+                    cex.lab=1, 
+                    cex.axis=1.1,
+                    cex.main=0.9,
+                    ylab="Absolute Frequency",
+                    xlab="Hamming distance",
+                    main=glue('no self-loops'))
+        #tmp$counts=tmp$counts/sum(tmp$counts)
+        axis(1, at=tmp$mids, labels=0:max(distances),cex.axis=1.1)
+dev.off()
+##########
 
 pdf(glue('DISTANCES_{type}.pdf'))
 par(mfrow=c(3,3), mai = c(0.53, 0.55, 0.14, 0))
